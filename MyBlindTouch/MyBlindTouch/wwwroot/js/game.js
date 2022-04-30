ea6afd5668303035d14e87cdc40613a805bedf2b
佑輔 伏木 <youfufumu@gmail.com>
2022-04-30 12:24:59 +0900

    jsが動かないエラー　解決

--- MyBlindTouch/MyBlindTouch/wwwroot/js/game.js
+++ MyBlindTouch/MyBlindTouch/wwwroot/js/game.js
@@ -1,31 +1,45 @@
 /*デフォルト値*/
 var countsec = 3;
 let FirstCountTimer;
-const QuizStr;
+//const QuizStr=require('./SettingsData.json');
+/*
+ fetch("./students.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => console.log(jsondata));
+ */ 
 let GameTime;
 let AnswerTime;
 let SoundVolume;
 let GameSound;
 /*id取得*/
 const secid = document.getElementById("sec");
-secid.innerText = String(countsec);
+const gametime = document.getElementById("gametime");
+
+/*class取得*/ 
+const StartCountModal = document.querySelector(".startcount-modal");
+const GameTimeArea = document.querySelector(".GameTimeArea");
+const GamePlayArea = document.querySelector(".GamePlayArea");
+
 
 /*メソッド*/
 //最初のカウントダウン
 function FirstCountDown() {
-    alert("firstcountdown");
-    //FirstCountTimer = setInterval(FirstCount, 1000);
+    FirstCountTimer = setInterval(FirstCount, 1000);
 }
 function FirstCount() {
-    alert(countsec);
+    secid.innerText = String(countsec);
     countsec--;
     if (countsec < 0) {
         secid.innerText = "";
-        firstcountmodal.classList.remove("open-modal");
+        StartCountModal.classList.remove("open-modal");
         clearInterval(FirstCountTimer);
+        GameTimeArea.classList.add("open-modal");
+        GamePlayArea.classList.add("open-modal");
+        GamePlay();
     }
 }
 //最初のカウントが終わったら問題と入力フォームを表示
-function time() {
-    alert("alert");
+function GamePlay() {
 }
