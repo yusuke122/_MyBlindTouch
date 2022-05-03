aca055347b0b7fc23f828f3dc2dbdb4927c72f14
佑輔 伏木 <youfufumu@gmail.com>
2022-05-03 14:48:28 +0900

    ゲーム結果の表示

--- MyBlindTouch/MyBlindTouch/wwwroot/js/game.js
+++ MyBlindTouch/MyBlindTouch/wwwroot/js/game.js
@@ -1,18 +1,14 @@
-/*デフォルト値*/
-let countsec = 3;
-let FirstCountTimer, AnswerTimeTimer, GameTimeTimer;
-let GameTime = 30;
-let LastGameTime = GameTime;
-let AnswerTime = 5;
-let AnswerTimeMs = AnswerTime * 1000;
-let SoundVolume=50;
-let GameSound = "soundon";
-let QuizStrOutputList;
 /*id取得*/
 const secid = document.getElementById("sec");//ゲーム開始時のカウントダウンを表示
 const gametime = document.getElementById("gametime");//ゲームの残り時間を表示
 const question = document.getElementById("question");//問題
 const answer = document.getElementById("answer");//解答フォーム：valueで解答を取り出す
+const questioncountid = document.getElementById("questioncount");//ゲームの結果を表示:出題数
+const answertruecountid = document.getElementById("answertruecount");//ゲームの結果を表示:正答数
+const answeraveragetimeid = document.getElementById("answeraveragetime");//ゲームの結果を表示:平均解答時間
+const playerlevel = document.getElementById("playerlevel");//ゲームの結果を表示:ステータス
+
+const gameresultdetail = document.getElementById("gameresult-detail");//ゲームの結果詳細を表示
 const focusBtn = document.getElementById("focus-button");//解答フォームにフォーカスを充てるためのボタン
 const PauseBtn = document.getElementById("pause-button");//一時停止ボタン
 const PauseReplayBtn = document.getElementById("pause-replay-button");//一時停止状態から再開するボタン
@@ -29,6 +25,24 @@ const PauseModal = document.querySelector(".pause-modal");
 const GameOverModal = document.querySelector(".gameover-modal");
 const GameResultModal = document.querySelector(".gameresult-modal");
 const GameResultDetailModal = document.querySelector(".gameresult-detail-modal");
+
+/*デフォルト値*/
+let countsec = 3;
+let FirstCountTimer, AnswerTimeTimer, GameTimeTimer;
+let GameTime = 30;
+let LastGameTime = GameTime;
+let AnswerTime = 5;
+let AnswerTimeMs = AnswerTime * 1000;
+let DefAnsTime = AnswerTimeMs;
+let SoundVolume = 50;
+let GameSound = "soundon";
+let QuizStrOutputList;
+
+let questionoutputlist = new Array(0);//誤答後に再度出力されたものを含める
+let answeroutputlist = new Array(0);//正答、誤答全て含める
+let answerjudgelist = new Array(0);//正答、誤答全て含める
+let answertimelist = new Array(0);//正答、誤答全て含める
+
 /*メソッド*/
 //最初のカウントダウン
 function FirstCountDown() {
@@ -96,11 +110,20 @@ answer.addEventListener("input", AnswerJudge);//入力時の正誤判定
 function AnswerJudge() {
     if (answer.value == question.value)//正解時
     {
+        questionoutputlist.push(question.value);
+        answerjudgelist.push('o');
+        answeroutputlist.push(answer.value);
+        answertimelist.push((Math.floor(DefAnsTime - AnswerTimeMs)) / 1000);
+
         clearTimeout(AnswerTimeTimer);
         AnswerTrue();
         setTimeout(() => { GamePlay(); answer.value = ''; }, 500);
     }
     else if (answer.value != question.value.slice(0, answer.value.length)) {//不正解時
+        questionoutputlist.push(question.value);
+        answerjudgelist.push('x');
+        answeroutputlist.push(answer.value);
+        answertimelist.push((Math.floor(DefAnsTime - AnswerTimeMs)) / 1000);
         AnswerFalse();
         setTimeout(() => { answer.value = ''; }, 500);
     }
@@ -124,6 +147,10 @@ PauseBtn.addEventListener("click", function () {
 //一時停止状態から再開する
 PauseReplayBtn.addEventListener("click", function () {
     PauseModal.classList.remove("open-modal");
+    setTimeout(() => {
+        GameTimeWatch();
+        GamePlay();
+    }, 50);
     GameTimeWatch();
     GamePlay();
 });
@@ -136,15 +163,74 @@ function GameOver() {
 }
 //結果の表示
 function GameResult() {
+    GameTimeModal.classList.remove("open-modal");
+    GamePlayModal.classList.remove("open-modal");
     GameOverModal.classList.remove("open-modal");
     GameResultModal.classList.add("open-modal");
+    /*
+     出題数、正答数、平均解答時間、プレイヤーのレベルを出力
+     */
+    var questioncount = 0;
+    var answertruecount = 0;
+    var answeraveragetime = 0;
+    var answertimesum = 0;
+    var answeraveragetimelist = new Array(0);
+    var falsequestions = new Array(0);
+    var i = 0;
+    var j = 0;
+    questionoutputlist.forEach(function (element) {
+        //出題数＝正答した問題＋時間切れの問題
+        //平均解答時間＝正答した問題の解答時間
+        if (answerjudgelist[i] == 'o') {
+            //正答時
+            questioncount++;
+            answertruecount++;
+            answeraveragetimelist.push(answertimelist[i]);
+        }
+        else {
+            //誤答時
+            if (answertimelist[i] == DefAnsTime) {
+                questioncount++;
+                answeraveragetimelist.push(answertimelist[i]);
+            }
+        }
+        i++;
+    });
+    questioncountid.innerText = "出題数：" + questioncount + "問";
+    answertruecountid.innerText = "正答数：" + answertruecount + "問";
+    answeraveragetimelist.forEach(function (element) {
+        answertimesum += answeraveragetimelist[j];
+        j++;
+    });
+    answeraveragetime = Math.floor((answertimesum / answeraveragetimelist.length) * 1000) / 1000;
+    answeraveragetimeid.innerText = "平均解答時間：" + answeraveragetime + "s";
+    //全出題に対しパーフェクトはマスター　ミスが10％未満はblindtouch上級者　20%未満はblindtouch中級者　それ以下は初心者
+    if (answertruecount / questionoutputlist.length == 1) {
+        playerlevel.innerText = "あなたは Blind Touch Master です";
+    }
+    else if (1 > (answertruecount / questionoutputlist.length) && (answertruecount / questionoutputlist.length) > 0.9) {
+        playerlevel.innerText = "あなたは　Blind Touch TopPlayer です";
+    }
+    else if (0.9 >= (answertruecount / questionoutputlist.length) && (answertruecount / questionoutputlist.length) > 0.8) {
+        playerlevel.innerText = "あなたは　Blind Touch Expert　です";
+    }
+    else {
+        playerlevel.innerText = "あなたは　Blind Touch Beginner　です";
+    }
 }
 //結果詳細の表示
 ResultDetailBtn.addEventListener("click", function () {
+    GameResultModal.classList.remove("open-modal");
     GameResultDetailModal.classList.add("open-modal");
-    //gameAroundTime = pauseToPlay;
+    var i = 0;
+   
+    questionoutputlist.forEach(function (element) {
+        i++;
+    });
+    gameresultdetail.innerText = html;
 });
 //詳細から結果画面へ
 DetailToResultBtn.addEventListener("click", function () {
     GameResultDetailModal.classList.remove("open-modal");
+    setTimeout(() => { GameResultModal.classList.add("open-modal") },1000);
 });
