7230a68756c07894acccc51ec1252ea1b83950f7
佑輔 伏木 <youfufumu@gmail.com>
2022-05-03 22:08:26 +0900

    before deploy

--- MyBlindTouch/MyBlindTouch/Program.cs
+++ MyBlindTouch/MyBlindTouch/Program.cs
@@ -1,33 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using MyBlindTouch.Data;
 using Azure.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
 builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-
-builder.Services.AddDbContext<MyBlindTouchContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyBlindTouchContext") ?? throw new InvalidOperationException("Connection string 'MyBlindTouchContext' not found.")));
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-//sessionを使うため記述
-//Microsoft.AspNetCore.Sessionパッケージ＝セッション状態を管理するためのミドルウェアを提供
-//ミドルウェアを利用するために以下二行が必要
-//ビルドの前に記述しないとエラー
-builder.Services.AddDistributedMemoryCache();//バックアップデータの設定
-//builder.Services.AddSession();//セッション機能の追加
-
-builder.Services.AddSession(options =>//セッション機能の追加
-{
-    options.IdleTimeout = TimeSpan.FromSeconds(10);//Idle状態の時間
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
 
 var app = builder.Build();
 
@@ -43,10 +21,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-//セッションを使う
-app.UseSession();
-
 
 app.UseAuthorization();
 
--- MyBlindTouch/MyBlindTouch/obj/Debug/net6.0/MyBlindTouch.AssemblyInfo.cs
+++ MyBlindTouch/MyBlindTouch/obj/Debug/net6.0/MyBlindTouch.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("134601bc-9692-44ec-8c6b-ed0be1f12367")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("b15c9423-85fc-4bcc-a078-fe37b4d019fb")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlindTouch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
