dd6523a1cbb2e5a4cc1ae9308f0da09f57a0d861
佑輔 伏木 <youfufumu@gmail.com>
2022-05-10 11:19:50 +0900

    add

--- MyBlindTouch/MyBlindTouch/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyBlindTouch.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
